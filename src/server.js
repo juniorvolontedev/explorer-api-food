5692e75ff17e8c79c364962103ea361a18ef4eee
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-18 08:55:16 -0300

    adicionado cors

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ require("dotenv/config");
 
 const AppError = require("./utils/AppError");
 
+const cors = require("cors");
 const express = require("express");
 const routes = require("./routes");
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(routes);
 
